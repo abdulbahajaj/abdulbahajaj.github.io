f8a3892e798c3bf9df484520daa69d70d2999124
abdulbahajaj <abdulbahajaj@abdulbaajajsMBP.hitronhub.home>
2019-07-04 04:01:46 -0600

    updated source

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header(){
+    return (
+        <div className="Header">
+            <div className="name">Abdul Bahajaj</div>
+            <div className="description">
+                This blog is the world's most trusted authority on programming, technology and life in general
+                <br/>
+                The views expressed in this blog don't necessary reflect anyone's opinion
+            </div>
+            <div className="links">
+                <a href="https://github.com/abdulbahajaj">Github</a>
+                <a href="https://www.linkedin.com/in/abdul-bahajaj-016a9337/" >Linkedin</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Navigation.js
@@ -0,0 +1,27 @@
+import React from 'react';
+function Article(props){
+    return (
+        <div className="article">
+            <div className="title">
+                {props.title}
+            </div>
+            <div className="description">
+                {props.description}
+            </div>
+        </div>
+    )
+}
+
+function Empty(){
+    return <div className="Empty">None of my wisdom is shared here. YET!</div>
+}
+
+function Navigation(){
+    return (
+        <div className="Navigation">
+            <Empty/>
+        </div>
+    )
+}
+
+export default Navigation;
